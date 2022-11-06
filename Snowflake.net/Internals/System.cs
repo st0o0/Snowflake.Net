b333b232fa0f08dd54b46b364f7e90b244722dc8
ST00 <jan.schloots@bluewin.ch>
2022-11-06 08:07:23 +0100

    ready to release

--- Snowflake.net/Internals/System.cs
+++ Snowflake.net/Internals/System.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Snowflake.net.Internals
+namespace Snowflake.Internals
 {
     internal static class System
     {
-        public static Func<long> currentTimeFun = InternalCurrentTimeMillis;
+        private static Func<long> currentTimeFunc = InternalCurrentTimeMillis;
 
+        public static long CurrentTimeMillis()
+        {
+            return currentTimeFunc();
+        }
 
+        private static readonly DateTime Jan1st1970 = new DateTime
+           (1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
-        private static readonly DateTime Jan1st1970 = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-
-        private static long InternalCurrentTimeMillis() 
+        private static long InternalCurrentTimeMillis()
         {
             return (long)(DateTime.UtcNow - Jan1st1970).TotalMilliseconds;
         }
--- Snowflake.net/Snowflake.cs
+++ Snowflake.net/Snowflake.cs
@@ -1,55 +1,84 @@
 ﻿using System;
 
-namespace Snowflake.net
+namespace Snowflake
 {
     public class Snowflake
     {
         private const long _epoch = 1288834974657L;
 
         private const int _workerIdBits = 5;
-        private const int _dataCenterIdBits = 5;
+        private const int _datacenterIdBits = 5;
         private const int _sequenceIdBits = 12;
-        private const long _maxWorkerId = -1L ^ (-1L << _workerIdBits);
-        private const long _maxDataCenterId = -1L ^ (-1L << _dataCenterIdBits);
+        private const long _maxWorkerId = -1L ^ -1L << _workerIdBits;
+        private const long _maxDataCenterId = -1L ^ -1L << _datacenterIdBits;
 
         private const int _workerIdShift = _sequenceIdBits;
-        private const int _dataCenterIdShift = _sequenceIdBits + _workerIdBits;
-        private const int _timestampLeftShift = _sequenceIdBits + _workerIdBits + _dataCenterIdBits;
-        private const long _squenceMask = -1L ^ (-1L << _sequenceIdBits);
+        private const int _datacenterIdShift = _sequenceIdBits + _workerIdBits;
+        private const int _timestampLeftShift = _sequenceIdBits + _workerIdBits + _datacenterIdBits;
+        private const long _squenceMask = -1L ^ -1L << _sequenceIdBits;
 
         private long _sequence = 0L;
         private long _lastTimestamp = -1L;
 
-        readonly object _lock = new Object();
+        readonly object _lock = new object();
 
         public Snowflake(long workerId, long dataCenterId, long sequence = 0L)
         {
-            this.WorkerId = workerId;
-            this.DataCenterId = dataCenterId;
-            this.Sequence = sequence;
+            WorkerId = workerId;
+            DataCenterId = dataCenterId;
+            Sequence = sequence;
         }
 
         public long WorkerId { get; protected set; }
 
         public long DataCenterId { get; protected set; }
-        
+
         public long Sequence { get; internal set; }
 
         public virtual long NextId()
         {
             lock (_lock)
             {
-                var timestamp = TimeGen();
+                long timestamp = TimeGen();
 
+                if (timestamp < _lastTimestamp)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
 
+                if (_lastTimestamp == timestamp)
+                {
+                    _sequence = _sequence + 1 & _squenceMask;
+                    if (_sequence == 0)
+                    {
+                        timestamp = TilNextMillis(_lastTimestamp);
+                    
+                    }
 
+                }
+                else
+                {
+                    _sequence = 0;
+                }
+                _lastTimestamp = timestamp;
+                return ((timestamp - _epoch) << _timestampLeftShift) | (_datacenterIdBits << _datacenterIdShift) | (_workerIdBits << _workerIdShift) | _sequence;
             }
         }
 
+        protected virtual long TilNextMillis(long lastTimestamp)
+        {
+            long timestamp = TimeGen();
+            while (timestamp <= lastTimestamp)
+            {
+                timestamp = TimeGen();
+            }
+            return timestamp;
+
+        }
 
         protected virtual long TimeGen()
         {
-            return System.CurrentTimeMillis();
+            return Internals.System.CurrentTimeMillis();
         }
     }
 }
